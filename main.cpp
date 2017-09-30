0379cf2bc6c9c08a6101115e30d6b946f2d6f9bd
NeverMine17 <dannevergame@gmail.com>
2017-09-30 08:04:52 +0400

    Несколько команд

--- main.cpp
+++ main.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Footballer.h"
+#include "OurTeam.h"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Window");
     window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(60);
 
-    int currentPlayer = 0;
-
-    std::vector<Footballer> Players = {
-            Footballer({5, 5}, 20, sf::Color::Red),
-            Footballer({6, 6}, 20, sf::Color::Red),
-            Footballer({7, 7}, 20, sf::Color::Red)
-    };
+    int currentPlayer = 0; // max = 20
 
     // Главный цикл приложения
+    auto current = PlayersRed.begin();
     while (window.isOpen()) {
         // Обрабатываем события в цикле
         sf::Event event;
         while (window.pollEvent(event)) {
+            if (currentPlayer == 20) { window.close(); }
             // Кроме обычного способа наше окно будет закрываться по нажатию на Escape
-            if (event.type == sf::Event::Closed ||
-                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+            if (event.type == sf::Event::Closed or
+                (event.type == sf::Event::KeyPressed and event.key.code == sf::Keyboard::Escape)) {
                 window.close();
             }
-            if (event.type == sf::Event::MouseMoved) {
-                if (currentPlayer < Players.size()) {
-                    Players[currentPlayer].mouseMove(event.mouseMove.x - Players[currentPlayer].getRadius(),
-                                                     event.mouseMove.y - Players[currentPlayer].getRadius());
-                } else {
-                    window.close();
-                }
-
+            if (event.type == sf::Event::MouseMoved and event.MouseEntered) {
+                current->mouseMove({event.mouseMove.x - current->radius,
+                                    event.mouseMove.y - current->radius}
+                );
             }
-            if (event.type == sf::Event::MouseButtonPressed) {
-                Players[currentPlayer].mouseClick();
-                if (currentPlayer < Players.size()) {
-                    currentPlayer++;
-                } else {
-                    window.close();
-                }
+            if (event.type == sf::Event::MouseButtonPressed and event.MouseEntered) {
+                //current->mouseClick(); // Be warned! Something may-be wrong here!
+                currentPlayer++;
+                current++;
+            }
+            if (currentPlayer == 10) {
+                current = PlayersBlue.begin();
             }
         }
         // Очистка
         window.clear();
-        for (auto b : Players) {
+        for (auto b : PlayersRed) {
+            window.draw(b);
+        }
+        for (auto b : PlayersBlue) {
             window.draw(b);
         }
         // Тут будут вызываться функции обновления и отрисовки объектов
